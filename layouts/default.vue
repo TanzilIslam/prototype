ed452ba9a8d7e2864cb6c6513d8980477f4ea366
Mac-01 <mac-01@Mac-01s-iMac.local>
2021-10-27 20:46:02 +0600

    up

---
+++ layouts/default.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-app dark>
+    <v-app-bar height="72" app flat>
+      <v-toolbar-title class="brand-title">MENSCRIPT</v-toolbar-title>
+      <v-app-bar-nav-icon class="mr-2" @click.stop="drawer = !drawer" />
+      <span class="account"> Account menu</span>
+      <!-- <v-img src="~/asstes/black.png"></v-img> -->
+    </v-app-bar>
+    <v-main class="body-class">
+         <Nuxt />
+    </v-main>
+    <v-footer :absolute="!fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: "mdi-apps",
+          title: "Welcome",
+          to: "/",
+        },
+        {
+          icon: "mdi-chart-bubble",
+          title: "Inspire",
+          to: "/inspire",
+        },
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: "Vuetify.js",
+    };
+  },
+};
+</script>
+<style scoped>
+.brand-title {
+  margin-left: 34px;
+  margin-left: 32px;
+  height: 21px;
+  font-family: "Work Sans" !important;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 21px;
+  letter-spacing: 3px;
+  color: #2a2a2a;
+}
+.account {
+  height: 18px;
+
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: normal;
+  font-size: 15px;
+  line-height: 18px;
+
+  color: #2a2a2a;
+}
+.body-class{
+  background: #e00000;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,50 @@
+<template>
+<div >
+  <div class="mt-4 mb-4 ml-14">
+    <p class="welcomm-text ">Welcome Rob</p>
+  <v-btn class="change-personal-detail-button"><div class="change-personal-detail-button-text">Change personal details</div> <v-icon class="ml-3">mdi-deskphone</v-icon></v-btn>
+  </div>
+  <div class="table mt-4">
+    <v-img :src="require('@/assets/graph.png')">
+    </v-img>
+  </div>
+</div>
+</template>
+<style scoped>
+.welcomm-text{
+  margin-top: 183px;
+width: 181px;
+height: 34px;
+font-family: "Inter";
+font-style: normal;
+font-weight: normal;
+font-size: 28px;
+line-height: 34px;
+color: #2A2A2A;
+}
+.change-personal-detail-button{
+  width: 299px;
+height: 59px !important;
+border: 0.5px solid #7A7A7A;
+box-sizing: border-box;
+
+}
+.change-personal-detail-button-text{
+
+height: 27px;
+
+
+font-family: "Inter";
+font-style: normal;
+font-weight: 600;
+font-size: 14px;
+line-height: 27px;
+/* identical to box height, or 180% */
+color: #2A2A2A;
+
+}
+.table img {
+  height: 174px;
+
+}
+</style>
